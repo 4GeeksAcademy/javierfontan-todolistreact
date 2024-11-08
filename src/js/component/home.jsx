52e13b5d77f0761473dd745184ba903ee2b54f28
jfontan93 <167186600+jfontan93@users.noreply.github.com>
2024-11-08 09:51:14 +0000

    javierfontan

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputText, setInput] = useState("");
+  const [toDo, setToDo] = useState([]);
+
+  function sendData(event) {
+    event.preventDefault();
+    if (inputText.trim() !== "") {
+      setToDo([...toDo, inputText.trim()]);//De expansión
+      setInput("");
+    }
+  }
+
+  function handleDelete(index) {
+    const updatedToDo = toDo.filter((_, i) => i !== index);
+    setToDo(updatedToDo);
+  }
+
+  return (
+    <div className="container">
+      <form onSubmit={sendData}>
+        <h1 className="Title text-center mt-5">To Do List</h1>
+        <div className="Lista">
+          <input type="text" className="InputCSS p-1 text-muted" placeholder="Qué necesitas hacer ?" value={inputText} onChange={(event) => setInput(event.target.value)} onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              sendData(event);
+            }
+          }} />
+          <ul className="list-group paper">
+            {toDo.length === 0 ? (
+              <li className="list-group-item bg-light text-muted mb-3">No hay tareas pendientes, añadir tareas</li>
+            ) : (
+              toDo.map((task, index) => (
+                <li key={index} className="list-group-item d-flex justify-content-between align-items-center text-muted">
+                  {task}
+                  <button
+                    type="button"
+                    className="btn p-0 m-0"
+                    onClick={() => handleDelete(index)}
+                  >
+                    <i class="fas fa-trash-alt" style={{ fontSize: '0.5rem' }}></i>
+                  </button>
+                </li>
+              ))
+            )}
+          </ul>
+          <p className="AmountTasks">
+            {toDo.length} {toDo.length === 1 ? "item left" : "items left"}
+          </p>
+        </div>
+      </form >
+    </div>
+  );
 };
 
 export default Home;
